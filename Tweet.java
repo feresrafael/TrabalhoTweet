db789fa94262524455aa4c960aa85582c2acb75b
feresrafael <rafael.feres@soulasalle.com.br>
2019-09-10 17:21:43 -0300

    Add files via upload

---
+++ Tweet.java
@@ -0,0 +1,28 @@
+
+package tweet;
+
+/**
+ *
+ * @author feres
+ */
+public class Tweet {
+    
+    private String texto;
+	
+	public Tweet(String mensagem) {
+		if (mensagem.length() >= 140) {
+			mensagem = mensagem.substring(0, 140);
+		}
+		else {
+			this.texto = mensagem;
+		}
+	}
+
+public String getTweet() {
+	return this.texto;
+}
+public void setTweet(String mensagem) {
+	this.texto = mensagem;
+}
+    
+}
